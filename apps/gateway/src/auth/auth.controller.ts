09c06d68c1a58309cdf19ddd8bbf224e0d299199
AmirMohammad Najafi <73462542+Armnajafi@users.noreply.github.com>
2025-06-10 23:26:12 +0330

    feat(gateway): add auth verify service to controller

--- apps/gateway/src/auth/auth.controller.ts
+++ apps/gateway/src/auth/auth.controller.ts
@@ -7,11 +7,19 @@ export class AuthController {
     @Inject('AUTH_SERVICE') private readonly authClient: ClientProxy,
   ) {}
 
-  @Post('request')
-  authRequest(@Body() body: { phone: string }) {
+  @Post('request-otp')
+  requestOtp(@Body() body: { phone: string }) {
     return this.authClient.send(
       { cmd: 'auth.request-otp' },
       { phone: body.phone },
+    );
+  }
+
+  @Post('verify-otp')
+  verifyOtp(@Body() body: { phone: string; otp: string }) {
+    return this.authClient.send(
+      { cmd: 'auth.verify-otp' },
+      { phone: body.phone, otp: body.otp },
     );
   }
 }
